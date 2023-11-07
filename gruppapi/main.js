6b7e6bc81c13ea1949aa3067dcfd0ea0d0ab27eb
Adamwadin <Adamwadin@live.se>
2023-11-07 15:38:49 +0100

    window onload

--- gruppapi/main.js
+++ gruppapi/main.js
@@ -1,6 +1,20 @@
 const kanyeQuoteDiv = document.getElementById("textContainer");
 const randomQuoteButton = document.getElementById("quoteButton");
 
+fetch("https://api.kanye.rest")
+  .then((response) => response.json())
+  .then((data) => {
+    const kanyeQuote = data.quote;
+
+    const quoteDiv = document.createElement("div");
+    quoteDiv.textContent = kanyeQuote;
+
+    kanyeQuoteDiv.appendChild(quoteDiv);
+  })
+  .catch((error) => {
+    console.error("du får inge kanye quote :()", error);
+  });
+
 randomQuoteButton.addEventListener("click", () => {
   kanyeQuoteDiv.innerHTML = "";
   fetch("https://api.kanye.rest")
