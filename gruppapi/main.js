9234761acc0661c5e412d01da675c623ef40870a
Adamwadin <Adamwadin@live.se>
2023-11-07 15:09:14 +0100

    1

--- gruppapi/main.js
+++ gruppapi/main.js
@@ -1,15 +1,19 @@
 const kanyeQuoteDiv = document.getElementById("textContainer");
+const randomQuoteButton = document.getElementById("quoteButton");
 
-fetch("https://api.kanye.rest")
-  .then((response) => response.json())
-  .then((data) => {
-    const kanyeQuote = data.quote;
+randomQuoteButton.addEventListener("click", () => {
+  kanyeQuoteDiv.innerHTML = "";
+  fetch("https://api.kanye.rest")
+    .then((response) => response.json())
+    .then((data) => {
+      const kanyeQuote = data.quote;
 
-    const quoteDiv = document.createElement("div");
-    quoteDiv.textContent = kanyeQuote;
+      const quoteDiv = document.createElement("div");
+      quoteDiv.textContent = kanyeQuote;
 
-    kanyeQuoteDiv.appendChild(quoteDiv);
-  })
-  .catch((error) => {
-    console.error("du får inge kanye quote :()", error);
-  });
+      kanyeQuoteDiv.appendChild(quoteDiv);
+    })
+    .catch((error) => {
+      console.error("du får inge kanye quote :()", error);
+    });
+});
